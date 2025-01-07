c59ae16d15bf1975cc30191f51ddd8e0ff86e53d
Thien Vu <thien.vu@gmail.com>
2025-01-07 15:21:59 -0800

    Ch03 - Finger Exercise 03

---
+++ ch03/finger-exercise-03.py
@@ -0,0 +1,15 @@
+# Finger exercise: Write a program that prints the sum of the prime
+# numbers greater than 2 and less than 1000. Hint: you probably want
+# to have a loop that is a primality test nested inside a loop that
+# iterates over the odd integers between 3 and 999
+
+sum = 0
+for i in range(3, 999, 2):
+    is_prime = True
+    for j in range(3, i):
+        if i % j == 0:
+            is_prime = False
+            break
+    if is_prime:
+        sum = sum + i
+print(sum)
