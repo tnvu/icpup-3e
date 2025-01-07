7ba55be5f41425b179b562e464865bbfedd9c47b
Thien Vu <thien.vu@gmail.com>
2025-01-07 14:05:06 -0800

    Ch02 - Finger Exercise 03

---
+++ ch02/finger-exercise-03.py
@@ -0,0 +1,15 @@
+# Finger exercise: Replace the comment in the following code with a
+# while loop.
+#     num_x = int(input('How many times should I print the letter
+#     X? '))
+#     to_print = ''
+#     #concatenate X to to_print num_x times
+#     print(to_print)
+
+num_x = int(input('How many times should I print the letter X? '))
+to_print = ''
+i = 0
+while (i < num_x):
+    to_print = to_print + 'X'
+    i = i + 1
+print(to_print)
