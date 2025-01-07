1feab8374f8adf5f659391c11f62c144fc5b1ddc
Thien Vu <thien.vu@gmail.com>
2025-01-07 12:54:27 -0800

    Chapter 1 Finger Exercise

---
+++ ch01/finger-exercise-01.py
@@ -0,0 +1,12 @@
+# Finger exercise: Computers can be annoyingly literal. If you don't
+# tell them exactly what you want them to do, they are likely to do the
+# wrong thing. Try writing an algorithm for driving between two
+# destinations. Write it the way you would for a person, and then
+# imagine what would happen if that person were as stupid as a
+# computer, and executed the algorithm exactly as written. (For an
+# amusing illustration of this, take a look at the video
+# https://www.youtube.com/watch?v=FN2RM-CHkuI&t=24s.)
+
+# 1. Get instructions
+# 2. Follow first instruction
+# 3. If at destination stop, else follow next instruction
