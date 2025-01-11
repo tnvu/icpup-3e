5fe13ec71d99fbe25f14853d162f26ca6f859454
Thien Vu <thien.vu@gmail.com>
2025-01-11 02:43:22 -0800

    Ch04 - Finger Exercise 05

---
+++ ch04/finger-exercise-04.py
@@ -0,0 +1,7 @@
+# Finger exercise: Write a function mult that accepts either one or
+# two ints as arguments. If called with two arguments, the function
+# prints the product of the two arguments. If called with one argument,
+# it prints that argument.
+
+def mult(multiplicand, multiplier=1):
+    return multiplicand * multiplier
