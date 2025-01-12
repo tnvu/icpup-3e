ef6440c4d6fe47f2ded2311eb0e54c2465d92e80
Thien Vu <thien.vu@gmail.com>
2025-01-11 23:01:34 -0800

    Ch04 - Finger Exercise 06

---
+++ ch04/finger-exercise-06.py
@@ -0,0 +1,7 @@
+# Finger exercise: Write a lambda expression that has two numeric
+# parameters. If the second argument equals zero, it should return
+# None. Otherwise it should return the value of dividing the first
+# argument by the second argument. Hint: use a conditional
+# expression.
+
+lambda x, y: None if y == 0 else x / y
