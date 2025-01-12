559052d3574760001e6bbbb0609037a897dd4ad0
Thien Vu <thien.vu@gmail.com>
2025-01-11 23:06:09 -0800

    Ch04 - Finger Exercise 07

---
+++ ch04/finger-exercise-07.py
@@ -0,0 +1,8 @@
+# Finger exercise: What does s.find(sub) return if sub does not
+# occur in s?
+
+# find(self, sub[, start[, end]], /) unbound builtins.str method
+#   Return the lowest index in S where substring sub is found, such that sub is contained within S[start:end].
+#
+#   Optional arguments start and end are interpreted as in slice notation.
+#   Return -1 on failure.
