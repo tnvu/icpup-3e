83173b73fd10120e31b658035d524692ee8d0489
Thien Vu <thien.vu@gmail.com>
2025-01-13 23:17:14 -0800

    Ch05 - Finger Exercise 04

---
+++ ch05/finger-exercise-04.py
@@ -0,0 +1,14 @@
+# Finger exercise: Implement a function satisfying the following
+# specification. Hint: it will be convenient to use lambda in the body of
+# the implementation.
+# def f(L1, L2):
+#   """L1, L2 lists of same length of numbers
+#   returns the sum of raising each element in L1
+#   to the power of the element at the same index in L2
+#   For example, f([1,2], [2,3]) returns 9"""
+
+def f(L1, L2):
+    """L1, L2 lists of the length of numbers
+    returns the sum of raising each element in L1
+    to the power of the element at the same index in L2"""
+    return sum(map(lambda x, y: x**y, L1, L2))
