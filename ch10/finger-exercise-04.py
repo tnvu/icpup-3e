b408670f7fe1457c533b3287879a92bb6de47866
Thien Vu <thien.vu@gmail.com>
2025-01-20 22:00:58 -0800

    Ch10 - Finger Exercise 04

---
+++ ch10/finger-exercise-04.py
@@ -0,0 +1,8 @@
+# Finger exercise: What is the value of the following expression?
+# isinstance('ab', str) == isinstance(str, str)
+
+# False:
+# isinstance('ab', str) is True because the first argument is a string
+# and the type is str
+# isinstance(str, str) is False because the first argument is not an
+# object but a type
