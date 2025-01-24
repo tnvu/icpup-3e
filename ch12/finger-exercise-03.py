602cc9b32b60987ca45f45b774396a7f3381a9ad
Thien Vu <thien.vu@gmail.com>
2025-01-24 00:45:47 -0800

    Ch12 - Finger Exercise 03

---
+++ ch12/finger-exercise-03.py
@@ -0,0 +1,5 @@
+# Finger exercise: Is merge_sort a stable sort?
+
+# merge_sort is a stable sort because the left most element
+# will continue to be branched into the left side of the split
+# and be left during the merge portion also
