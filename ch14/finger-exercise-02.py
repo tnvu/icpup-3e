82260f6a42c60bdeea747f31f1e7dbbe1fff814a
Thien Vu <thien.vu@gmail.com>
2025-01-29 04:30:16 -0800

    Ch14 - Finger Exercise 02

---
+++ ch14/finger-exercise-02.py
@@ -0,0 +1,6 @@
+# Finger exercise: Consider a digraph with weighted edges. Is the
+# first path found by BFS guaranteed to minimize the sum of the
+# weights of the edges?
+
+# No because the first path found by BFS is the minimum number
+# of edges, not the lowest sum of the weights of the edges.
