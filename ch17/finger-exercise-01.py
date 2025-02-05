2a4b498b2d7737a785bfd7d87b9d7365d228886f
Thien Vu <thien.vu@gmail.com>
2025-02-05 00:34:47 -0800

    Ch17 - Finger Exercise 01

---
+++ ch17/finger-exercise-01.py
@@ -0,0 +1,8 @@
+# Finger exercise: Andrea averages 5 strokes a hole when she plays
+# golf. One day, she took 40 strokes to complete the first nine holes.
+# Her partner conjectured that she would probably regress to the mean
+# and take 50 strokes to complete the next nine holes. Do you agree
+# with her partner?
+
+# No. She is more likely to be closer to 45 strokes on the next 9 holes.
+# Her partner has fallen for the gambler's fallacy.
