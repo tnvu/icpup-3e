309cec1caf1039abef679c969a3761c47f039160
Thien Vu <thien.vu@gmail.com>
2025-02-09 00:06:45 -0800

    Ch17 - Finger Exercise 03

---
+++ ch17/finger-exercise-03.py
@@ -0,0 +1,7 @@
+# Finger exercise: In Figure 17-19, why are the bins near the middle
+# of the histogram taller than the bins near the sides? Hint: think
+# about why 7 is the most common outcome of rolling a pair of dice.
+
+# Bins near the middle of the histogram are taller than the bins near the 
+# sides because there are more pairs of numbers between 0-100 that add up to
+# the numbers in the middle bins
