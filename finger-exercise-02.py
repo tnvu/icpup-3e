e6c482449fd160b7452b9fc49b2dad93d48f69db
Thien Vu <thien.vu@gmail.com>
2025-03-09 23:35:22 -0700

    Ch21 - Finger Exercise 02

---
+++ finger-exercise-02.py
@@ -0,0 +1,9 @@
+# Finger exercise: Estimate the probability that a randomly chosen
+# American is both male and weighs more than 197 pounds. Assume
+# that 50% of the population is male, and that the weights of the male
+# population are normally distributed with a mean of 210 pounds and a
+# standard deviation of 30 pounds. (Hint: think about using the
+# empirical rule.)
+
+# P(male) * P(weight > 197 | male) = 0.5 * 0.84
+#                                  = 0.42
