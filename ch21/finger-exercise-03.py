87e969696483f5bd1f65a0ac1a9dde0bb54ccac3
Thien Vu <thien.vu@gmail.com>
2025-03-11 02:08:30 -0700

    Move Finger Exercises for Ch21 into correct directory

---
+++ ch21/finger-exercise-03.py
@@ -0,0 +1,17 @@
+# Finger exercise: You are wandering through a forest and see a
+# field of delicious-looking mushrooms. You fill your basket with them,
+# and head home prepared to cook them up and serve them to your
+# husband. Before you cook them, however, he demands that you
+# consult a book about local mushroom species to check whether they
+# are poisonous. The book says that 80% of the mushrooms in the local
+# forest are poisonous. However, you compare your mushrooms to the
+# ones pictured in the book, and decide that you are 95% certain that
+# your mushrooms are safe. How comfortable should you be about
+# serving them to your husband (assuming that you would rather not
+# become a widow)?
+
+# A = mushroom is safe
+# B = confident that mushroom is safe
+# P(A|B) = P(B|A) * P(A) / P(B)
+#        = (0.95 * 0.2 + 0.05 * 0.8) * 0.2 / 0.95
+#        =~ 0.0484
