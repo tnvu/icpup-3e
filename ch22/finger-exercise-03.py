9a3e1ce5a3752df37abcfa8e6003d48b952ac6c2
Thien Vu <thien.vu@gmail.com>
2025-03-11 23:52:09 -0700

    Ch22 - Finger Exercise 03

---
+++ ch22/finger-exercise-03.py
@@ -0,0 +1,10 @@
+# Finger exercise: The infection-fatality rate for a disease is the
+# number of people who contract the disease divided by the number of
+# those people who die from the disease. The case-fatality rate for a
+# disease is the number of people who are diagnosed with the disease
+# divided by the number of those people who die from the disease.
+# Which of these is easier to estimate accurately, and why?
+
+# The case-fatality rate is easier to estimate because you have a confirmed
+# diagnosis. Some number of people will never be diagnosed with the disease
+# and yet will die from the disease.
