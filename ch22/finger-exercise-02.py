1a2fc21afdb8b5a4e19f2004ae17fd091fde42e8
Thien Vu <thien.vu@gmail.com>
2025-03-11 23:45:07 -0700

    Ch22 - Finger Exercise 02

---
+++ ch22/finger-exercise-02.py
@@ -0,0 +1,9 @@
+# Finger exercise: Over the last 100 years, the number of deaths per
+# year in Canada was positively correlated with the amount of meat
+# consumed per year in Canada. What lurking variable might explain
+# this?
+
+# The number of deaths per year in Canada being positively correlated
+# with the amount of meat consumed per year in Canada could be explained
+# by a larger population increasing both the number of deaths as well
+# as the amount of meat consumed.
