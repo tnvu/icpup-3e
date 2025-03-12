7b946088f2c056de241db0c6d08c80cf1bb58ab5
Thien Vu <thien.vu@gmail.com>
2025-03-12 00:12:56 -0700

    Ch22 - Finger Exercise 04

---
+++ ch22/finger-exercise-04.py
@@ -0,0 +1,14 @@
+# Finger exercise: On May 19, 2020, the New York Times reported a
+# 123% increase in U.S. air travel in a single month (from 95,161
+# passengers to 212,508 passengers). It also reported that this increase
+# followed a recent 96% drop in air travel. What was the total net
+# percentage change?
+
+# (212508 - 95161) / 95161 = 1.23 = 123%
+# (95161 - X) / X = -0.96
+# 95161 - X = -0.96X
+# 95161 = 0.04X
+# X = 95161 / 0.04 = 2379025
+
+# Total net percentage change
+# (212508 - 2379025) / 2379025 = -0.91 = -91%
